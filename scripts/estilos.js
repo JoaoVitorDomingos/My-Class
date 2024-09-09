f58a037b82cc079ad048c187a4cb6035671123e4
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2024-09-09 20:17:37 -0300

    Troca de Layout
    Adição de linhas para remover as classes "opened" e "closed", e para adição da classe "closed" quando line view for ativado

--- scripts/estilos.js
+++ scripts/estilos.js
@@ -16,6 +16,7 @@ const line_view = document.querySelector("#line-view")
 const container_sessoes = document.querySelectorAll("div.container-atividades")
 let icones_layout = [grid_view, line_view]
 
+let atividades = document.querySelectorAll(".atividade")
 let line_view_ativada = false
 
 //console.log(container_sessoes)
@@ -28,20 +29,23 @@ icones_layout.forEach(el => {el.addEventListener("click", evt => {
 
     if(evt.target.id == "grid-view") {
         container_sessoes.forEach(container => {
+            atividades.forEach(el => el.classList.remove("closed"))
+            atividades.forEach(el => el.classList.remove("opened"))
             container.classList.remove("line-view")
             container.classList.add("grid-view")
+
         })
     } else {
         container_sessoes.forEach(container => {
             container.classList.remove("grid-view")
             container.classList.add("line-view")
+            atividades.forEach(el => el.classList.add("closed"))
             line_view_ativada = true
         })
     }
 })})
 
 // Expande a atividade do layout line view
-// let atividades = document.querySelectorAll(".atividade")
 
 // console.log(atividades)
 
