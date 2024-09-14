867fcdda41a06b788b26904725d90b4af4fb85c0
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2024-09-14 17:53:14 -0300

    Abrir e Fechar Atividade no Line View

--- scripts/estilos.js
+++ scripts/estilos.js
@@ -19,6 +19,7 @@ let icones_layout = [grid_view, line_view]
 let atividades = document.querySelectorAll(".atividade")
 let line_view_ativada = false
 
+console.log(atividades)
 //console.log(container_sessoes)
 
 icones_layout.forEach(el => {el.addEventListener("click", evt => {
@@ -45,14 +46,53 @@ icones_layout.forEach(el => {el.addEventListener("click", evt => {
     }
 })})
 
+console.log("CLASS NOME:")
+console.log(grid_view.className)
+
 // Expande a atividade do layout line view
+let icones_editar_atividades = document.querySelectorAll("div.atividade>div>span")
+let atividade_p = document.querySelectorAll("div.atividade>p")
+let elementos_stop = [...icones_editar_atividades, ...atividade_p]
+//console.log(elementos_stop)
 
-// console.log(atividades)
+let nome_classe = /[atividade]/
 
-// atividades.forEach(el => el.addEventListener("click", evt => {
-//     if(line_view_ativada) {
-//         console.log("Fui clicado enquanto line view ativado")
-//         console.log(evt.target)
-//         evt.target.classList.toggle("opened")
-//     }
-// }))
+elementos_stop.forEach(icone => icone.addEventListener("click", evt => evt.stopPropagation()))
+
+atividades.forEach(el => el.addEventListener("click", evt => {
+    if(line_view_ativada) {
+        //console.log("Fui clicado enquanto line view ativado")
+        //console.log("Alvo:")
+        //console.log(evt.target)
+        //console.log(evt.target.className)
+        if(evt.target.nodeName == "DIV" && nome_classe.test(evt.target.className)) {
+            //console.log("É o elemento certo")
+        } else {
+            //console.log("NÃO é o elemento certo")
+            //console.log("Pai:")
+            //console.log(evt.target.parentNode)
+            //console.log(evt.target.parentNode.className)
+
+            let terminar = false
+            let pai = evt.target.parentNode
+            while(!terminar) {
+                if(pai.parentNode.nodeName == "DIV" & nome_classe.test(pai.className)) {
+                    //console.log("É o elemento certo")
+                    //console.log(pai)
+                    //console.log(pai.className)
+                    terminar = true
+
+                    pai.classList.toggle("closed")
+                    pai.classList.toggle("opened")
+                } else {
+                    //console.log("NÃO é o elemento certo")
+                    //console.log(pai)
+                    //console.log("Pai:")
+                    //console.log(pai.parentNode)
+                    //console.log(pai.parentNode.className)
+                    pai = pai.parentNode
+                }
+            }
+        }
+    }
+}))
