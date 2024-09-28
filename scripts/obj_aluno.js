deb5214f0f030e04ed9c52a681856d374e2ee2e4
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2024-09-28 14:44:00 -0300

    Criação do Script-Mudulo do Objeto Aluno

---
+++ scripts/obj_aluno.js
@@ -0,0 +1,10 @@
+export default class Aluno {
+    constructor(pnome, pnasc, psexo, pcpf, pend, pfoto) {
+        this.nome = pnome
+        this.nascimento = pnasc
+        this.sexo = psexo
+        this.cpf = pcpf
+        this.endereco = pend
+        this.foto = pfoto
+    }
+}
