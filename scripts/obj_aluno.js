1cdb633fa57ccfaba584590176793785049da83d
João Vitor Capazório Domingos <127691953+JoaoVitorDomingos@users.noreply.github.com>
2024-09-28 15:15:29 -0300

    Correções
    Modal Veja Mais - Adicionado o numero da matricula
    
    Script Modulo - adicionando a propriedade matricula

--- scripts/obj_aluno.js
+++ scripts/obj_aluno.js
@@ -1,10 +1,11 @@
 export default class Aluno {
-    constructor(pnome, pnasc, psexo, pcpf, pend, pfoto) {
+    constructor(pnome, pnasc, psexo, pcpf, pend, pfoto, pid) {
         this.nome = pnome
         this.nascimento = pnasc
         this.sexo = psexo
         this.cpf = pcpf
         this.endereco = pend
         this.foto = pfoto
+        this.matriculo = pid
     }
 }
